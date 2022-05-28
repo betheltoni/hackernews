5a4d16069b250f49ecccea8ce43ad29796d3e9bc
bethel enunwa <enunwabethel@gmail.com>
2022-05-28 08:57:11 +0100

    fixing routing issue

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.scss';
 import TopStories from './TopStories';
 
-import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom'
+import {  HashRouter, NavLink, Route, Routes } from 'react-router-dom'
 import LatestStories from './LatestStories';
 import BestStories from './BestStories';
 
@@ -12,18 +12,18 @@ function App() {
   return (
     <div className="App">
       <h1>Hackers News Clone</h1>
-      <BrowserRouter>
+      <HashRouter>
       <nav>
-        <NavLink to="/hackernews/">TOP STORIES</NavLink>
-        <NavLink to="/hackernews/latest">LATEST STORIES</NavLink>
-        <NavLink to="/hackernews/best">BEST STORIES</NavLink>
+        <NavLink to="/">TOP STORIES</NavLink>
+        <NavLink to="/latest">LATEST STORIES</NavLink>
+        <NavLink to="/best">BEST STORIES</NavLink>
       </nav>
         <Routes>
-          <Route path='/hackernews/' element={<TopStories/>} />
-          <Route path='/hackernews/latest' element={<LatestStories/>} />
-          <Route path='/hackernews/best' element={<BestStories/>} />
+          <Route path='/' element={<TopStories/>} />
+          <Route path='/latest' element={<LatestStories/>} />
+          <Route path='/best' element={<BestStories/>} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
